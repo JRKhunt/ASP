76199e5e30f78d88a4ae1e1bd0dc8ff584023c99
unknown <khuntjay6273@gmail.com>
2024-07-08 09:07:19 +0530

    gfdsf

---
+++ WebApplication1/WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public string CITY { get; set; }
+    }
+}
